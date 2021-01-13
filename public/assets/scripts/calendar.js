ed85726f1658d2b1bd2c247b0fadf8c117cfa510
Guilherme Ferreti <guiferretis@gmail.com>
2021-01-12 23:16:15 -0300

    Dia 2

--- public/assets/scripts/calendar.js
+++ public/assets/scripts/calendar.js
@@ -1,13 +1,38 @@
-import { endOfMonth, endOfWeek, format, startOfWeek } from 'date-fns';
-import { startOfMonth } from 'date-fns/esm';
+import { endOfMonth, endOfWeek, format, startOfWeek, startOfMonth, differenceInSeconds, addDays, addMonths, subMonths } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+
+document.querySelectorAll('#schedules-new').forEach(page => {
+
+    const form = page.querySelector('form');
+    const input = page.querySelector('[name=schedule_at]');
+    const button = form.querySelector('[type=submit]');
+
+    // input.addEventListener('change', e => button.disabled = !e.target.value);
+    
+    input.addEventListener('change', e => {
+
+        if (e.target.value) {
+            button.disabled = false;
+        } else {
+            button.disabled = true;
+        }
+    });
+
+    form.addEventListener('submit', e => {
+
+        if (!page.querySelector('[name=schedule_at]').value) {
+            button.disabled = true;
+            e.preventDefault();
+        }
+    });
+});
 
 document.querySelectorAll('.calendar').forEach(calendar => {
 
     const today = new Date();
-    const startMonth = startOfMonth(today);
-    const startAt = startOfWeek(startOfMonth);
-    const endAt = endOfWeek(endOfMonth(today));
+    let startMonth = startOfMonth(today);
+    let startAt = startOfWeek(startMonth);
+    let endAt = endOfWeek(endOfMonth(today));
 
     const title = calendar.querySelector('h2');
     const days = calendar.querySelector('.days');
@@ -17,14 +42,26 @@ document.querySelectorAll('.calendar').forEach(calendar => {
 
     btnToday.addEventListener('click', e => {
 
+        startMonth = startOfMonth(today);
+        startAt = startOfWeek(startMonth);
+        endAt = endOfWeek(endOfMonth(startMonth));
+        render();
     });
 
     btnPrev.addEventListener('click', e => {
-        
+
+        startMonth = subMonths(startMonth, 1);
+        startAt = startOfWeek(startMonth);
+        endAt = endOfWeek(endOfMonth(startMonth));
+        render();
     });
 
     btnNext.addEventListener('click', e => {
         
+        startMonth = addMonths(startMonth, 1);
+        startAt = startOfWeek(startMonth);
+        endAt = endOfWeek(endOfMonth(startMonth));
+        render();
     });
 
     const render = () => {
@@ -32,6 +69,66 @@ document.querySelectorAll('.calendar').forEach(calendar => {
         title.innerHTML = format(startMonth, 'MMMM yyyy', {
             locale: ptBR
         });
+
+        days.innerHTML = '';
+
+        let currentDay = new Date(startAt.getTime());
+
+        while (differenceInSeconds(endAt, currentDay) > 0) {
+
+            const li = document.createElement('li');
+
+            li.innerHTML = format(currentDay, 'd');
+            li.dataset.date = format(currentDay, 'yyyy-MM-dd');
+
+            if (format(currentDay, 'yyyyMMdd') < format(today, 'yyyyMMdd')) {
+
+                li.classList.add('month-prev');
+                li.style.backgroundColor = '#DDDDDD';
+                li.style.cursor = 'no-drop';
+            } else {
+
+                if (format(currentDay, 'yyyyMM') < format(startMonth, 'yyyyMM')) {
+    
+                    li.classList.add('month-prev');
+    
+                } else if (format(currentDay, 'yyyyMM') > format(startMonth, 'yyyyMM')) {
+    
+                    li.classList.add('month-next');
+    
+                } else if (format(currentDay, 'yyyyMMdd') === format(today, 'yyyyMMdd')) {
+    
+                    li.classList.add('active');
+                }
+    
+                li.addEventListener('click', e => {
+    
+                    // const target = e.target;
+                    const { target } = e;
+                    const selected = calendar.querySelector('.selected');
+    
+                    if (selected) {
+    
+                        selected.classList.remove('selected');
+                    }
+    
+                    target.classList.add('selected');
+    
+                    document.querySelector('[name=schedule_at]').value = target.dataset.date;
+    
+                    const evt = document.createEvent('HTMLEvents');
+    
+                    evt.initEvent('change', false, true);
+    
+                    document.querySelector('[name=schedule_at]').dispatchEvent(evt);
+                });
+            }
+
+
+            days.append(li);
+
+            currentDay = addDays(currentDay, 1);
+        }
     }
 
     render();
